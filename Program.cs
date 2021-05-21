5171645abd6c91cdd0ca6efbbb132e9dc2f24d3e
skybreaker76 <skybreaker76@gmail.com>
2021-05-21 16:11:54 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bongo_Wizard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bongo Wizard!");
+        }
+    }
+}
