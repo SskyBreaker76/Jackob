4e9b5b6b55451f3d6736aeded899c374434f7971
skybreaker76 <skybreaker76@gmail.com>
2021-05-24 13:05:14 +1200

    Z

--- Program.cs
+++ Program.cs
@@ -2,15 +2,18 @@
 
 namespace Bongo_Wizard
 {
-    class Program
+    public class ProgramSettings
     {
+        public char wizardChar = 'O';
+        
+    }
+
+    private class Program
+    {        
+        public static ProgramSettings settings;
+
         static void Main(string[] args)
         {
-            // JACKIES CODE
-            // TOUCH AND YOU DIE
-            // TOUCHED BY JAKOB
-            // RIP JAKOB, WHO DIED ON THE 21st OF MARCH 2021
-            Console.WriteLine("Bongo Wizard!");
         }
     }
 }
